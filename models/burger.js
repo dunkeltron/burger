40d04d99a1efef225e863ded67049ba00f9f5f84
Ian Dunkelberg <idunkelberg@gmail.com>
2018-11-15 16:20:06 -0800

    burger.js set up.

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,28 @@
 var orm = require("../config/orm.js");
 // *  Create the functions that call the ORM functions
+var burger = {
+    all: function(cb){
+        orm.selectAll("burgers",function(res){
+            cb(res);
+        });
+    },
 
+    create: function(cols, vals,cb){
+        orm.insertOne("burgers",cols,vals,cb,function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals,condition,cb){
+        orm.updateOne("burgers",objColVals,condition,cb,function(res){
+            cb(res);
+        });
+    },
+    delete: function(condition,cb){
+        orm.deleteOne("burgers",condition,function(res){
+            cb(res);
+        });
+    }
+}
 // * Export at the end of the `burger.js` file.
+
+module.exports = burger;
