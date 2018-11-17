fd6f8f0d4fb64d9b3c15e0a88f4a14225df50236
Ian Dunkelberg <idunkelberg@gmail.com>
2018-11-16 20:39:35 -0800

    More fixes to get heroku working.

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 //connection info for mysql 
-var connection
+var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
