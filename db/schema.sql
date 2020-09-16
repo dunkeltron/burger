8028e2592879070679ab81313455d3bce22d5a89
Ian Dunkelberg <idunkelberg@gmail.com>
2020-09-15 17:12:44 -0700

    npm updating packages

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,14 @@ CREATE TABLE burgers(
     devoured boolean,
     PRIMARY KEY (id)
 )
+
+DROP DATABASE IF EXISTS photo_db;
+CREATE DATABASE photo_db;
+USE photo_db;
+CREATE TABLE user (
+  `userId` INT NOT NULL AUTO_INCREMENT,
+  `password` VARCHAR(45) NULL,
+  `userName` VARCHAR(45) NULL,
+  PRIMARY KEY (`userId`));
+  
+  SELECT * FROM Users;
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,5 @@ INSERT INTO burgers (burger, devoured) VALUES ("Hamburger", FALSE);
 INSERT INTO burgers (burger,devoured) VALUES ("Cheeseburger", FALSE);
 
 INSERT INTO burgers (burger,devoured) VALUES ("Bacon Cheeseburger",FALSE);
+
+SELECT * FROM burgers;
